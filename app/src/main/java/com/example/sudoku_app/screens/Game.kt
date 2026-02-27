afe2c431c3c50c8362e991d37a3320cd928c59f5
Issa Khafaga <ikhafaga@live.com>
2026-02-27 16:57:54 +0000

    style: layout change on screen landscape orientation

--- app/src/main/java/com/example/sudoku_app/screens/Game.kt
+++ app/src/main/java/com/example/sudoku_app/screens/Game.kt
@@ -1,16 +1,25 @@
 package com.example.sudoku_app.screens
 
+import android.content.res.Configuration
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.displayCutoutPadding
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -24,6 +33,19 @@ fun GameScreen(
     modifier: Modifier = Modifier,
     sudokuViewModel: SudokuViewModel = viewModel()
 ) {
+    val isLandscape = LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
+
+    if(isLandscape){
+        Row(modifier.systemBarsPadding().displayCutoutPadding().fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Row(modifier.padding(10.dp).weight(4F), horizontalArrangement = Arrangement.Center) {
+                Grid(sudokuViewModel = sudokuViewModel)
+            }
+            VerticalDivider(thickness = 2.dp)
+            Row(modifier.width(150.dp).padding(0.dp).fillMaxHeight().weight(1F), verticalAlignment = Alignment.CenterVertically) {
+                NumberPad(sudokuViewModel = sudokuViewModel)
+            }
+        }
+    } else {
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -47,7 +69,7 @@ fun GameScreen(
             .padding(top = 16.dp)) {
             NumberPad(sudokuViewModel = sudokuViewModel)
         }
-    }
+    }}
 }
 
 @Preview(showBackground = true)
